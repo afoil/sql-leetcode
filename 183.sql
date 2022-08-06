d0549257bd53fea741c93a612ca928a20ac95edb
afoil <oakira114@gmail.com>
2022-08-07 01:46:16 +0900

    submitted

---
+++ 183.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select
+    customers.name as Customers
+from
+    customers
+where
+    customers.id not in (
+        select
+            customerId
+        from
+            orders
+    );
