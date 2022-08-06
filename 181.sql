f763e9b5e72cc5fc6bcb64bffbbf215c23f4caca
afoil <oakira114@gmail.com>
2022-08-07 01:46:10 +0900

    add comma

--- 181.sql
+++ 181.sql
@@ -5,4 +5,4 @@ from
     Employee as M
 where
     E.ManagerId = M.Id
-    and E.salary > M.salary
+    and E.salary > M.salary;
--- 182.sql
+++ 182.sql
@@ -5,4 +5,4 @@ from
 group by
     email
 having
-    count(email) > 1
+    count(email) > 1;
